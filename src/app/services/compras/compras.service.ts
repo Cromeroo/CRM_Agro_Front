a3bd16ebda6e4e442a5d9d5df8e692a3cbd255c5
Cromeroo <cromeroo@unal.edu.co>
2024-06-12 00:31:11 -0500

    vercel Dios ayuda

--- src/app/services/compras/compras.service.ts
+++ src/app/services/compras/compras.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
-const API_URL = environment.base_url + '/producto/compras';
+const API_URL = environment.base_url + '/producto/compras/';
 
 @Injectable({
   providedIn: 'root',
